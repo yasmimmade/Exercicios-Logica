0b1eec0e7ac248fb3dd11487d02126ad23b290eb
yasmimmade <yasmin.santos@madeinweb.app>
2022-05-24 12:07:41 -0300

    Exercicio 8 atualizado

--- exercicioOito.java
+++ exercicioOito.java
@@ -1,8 +1,5 @@
 package programas;
 
-import java.util.Random;
-import java.util.Scanner;
-
 //Faça um programa que voce passe uma senha de 3 digitos númericos, e o seu programa sejá capaz, 
 //de identificar essa senha
 // Ex: Imagine que vc tenha uma senha do facebook, de 3 digitos
@@ -11,24 +8,31 @@ import java.util.Scanner;
 public class exercicioOito {
 
     public static void main(String args[]) {
-    int senha = 327;
-        // 000
-        // 001
-        // 002
-        // ...
-        // 010
-        // 011
+        int senha = 321;
+        String tente;
+        String[] tentativas = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        boolean controle;
 
-        for (int i=0; i<400; i++){
-            int senha2 = i+1;
-            System.out.println(senha2);
-            if (senha2 == senha){
-            System.out.println(senha);
-            break;
+        for (int i = 0; i < 10; i++) {
+            controle = true;
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    tente = tentativas[i] + tentativas[j] + tentativas[k];
+                    int tenteConvertido = Integer.valueOf(tente);
+                    System.out.println(tente);
+                    if (tenteConvertido == senha) {
+                        System.out.println(senha + " acesso permitido");
+                        controle = false;
+                        break;
+                    }
+                }
+                if (controle == false) {
+                    break;
+                }
+            }
+            if (controle == false) {
+                break;
             }
         }
-
-
-        
     }
 }
